76fc4638c60b59f04fc78e3e00a15186a1d861e6
Malo <maloj@duck.com>
2025-05-26 13:29:41 +0200

    new graphs

--- archiBeaulieu/ring_length.R
+++ archiBeaulieu/ring_length.R
@@ -6,6 +6,8 @@ library(stringr)
 anat_long <- read.csv(file = "anat_long.csv", header = T, stringsAsFactors = F)
 anat_long <- anat_long %>%
   select(-X)
+metadata <- read.csv(file = "metadata.csv", header = T, stringsAsFactors = F)
+
 
 species <- c("Ho", "Fe", "Ft")
 
@@ -19,7 +21,19 @@ anat_sp_organ <- imap(anat_sp, function(df, name) {
 }) %>%
   flatten()
 
+anat_0to1 <- anat_sp_organ %>%
+  map(~mutate(.x,
+              ring_length_n = (ring_length_µm - min(ring_length_µm, na.rm = TRUE)) / (max(ring_length_µm, na.rm = TRUE) - min(ring_length_µm, na.rm = TRUE)),
+              mean_vessel_size_n = (mean_vessel_size_µm2 - min(mean_vessel_size_µm2, na.rm = TRUE)) / (max(mean_vessel_size_µm2, na.rm = TRUE) - min(mean_vessel_size_µm2, na.rm = TRUE)),
+              vessel_density_n = (vessel_density_per_mm2 - min(vessel_density_per_mm2, na.rm = TRUE)) / (max(vessel_density_per_mm2, na.rm = TRUE) - min(vessel_density_per_mm2, na.rm = TRUE)),
+              Kth_n = (Kth - min(Kth, na.rm = TRUE)) / (max(Kth, na.rm = TRUE) - min(Kth, na.rm = TRUE))
+              )
+      )
+  
 
+##################
+#####per year#####
+##################
 #####Ring length####
 
 
@@ -51,11 +65,9 @@ plot_ring_length_by_year <- function(data, year_col = "year", value_col = "ring_
   
   title_text <- paste(species_name, "–", organ_name, ": Ring length per year according to site type")
   
-  # Ensure year is a factor with levels in order
   data <- data %>%
     mutate(!!year_col := factor(.data[[year_col]], levels = c(2021, 2022, 2023, 2024)))
   
-  # Generate plot
   ggplot(data, aes(x = .data[[year_col]], y = .data[[value_col]])) +
     geom_boxplot() +
     facet_wrap(as.formula(paste("~", facet_col)), scales = "free_x", nrow = 1) +
@@ -242,4 +254,104 @@ plot_conductivity_by_year(anat_sp_organ$Ft_R)
 
 plot_conductivity_by_year(anat_sp_organ$Ho_T)
 plot_conductivity_by_year(anat_sp_organ$Ho_R)
+
+
+##################
+#####all years####
+##################
+anat_all_years <- anat_sp_organ %>%
+  map(~mutate(.x, 
+              sizeXcount = mean_vessel_size_µm2 * count_vessel,
+              densityXcount = vessel_density_per_mm2 * count_vessel,
+              KthXcount = Kth * count_vessel
+              )
+      ) %>%
+  map(~group_by(.x, number)) %>%
+  map(~summarize(.x,
+                 vessel_total = sum(count_vessel),
+                 ring_length_µm = sum(ring_length_µm),
+                 mean_vessel_size_µm2 = sum(sizeXcount) / sum(vessel_total),
+                 vessel_density_per_mm2 = sum(densityXcount) / sum(vessel_total),
+                 Kth = sum(KthXcount) / sum(vessel_total)
+                   )
+      ) %>%
+  map(~mutate_if(.x, is.integer, as.character)) %>%
+  map(~mutate(.x, number = ifelse(nchar(number) == 1, paste0("0", number), number))) %>%
+  map(~left_join(.x, metadata, by = "number"))
+
+
+#Make a dataset for stem and root data:
+anat_T <- anat_0to1[names(anat_sp_organ) %>% str_detect("_T")] %>%
+  bind_rows(.id = "source")
+anat_T <- anat_T %>%
+  group_by(site_type, species, year) %>%
+  summarize(ring_length = mean(ring_length_µm, na.rm=TRUE),
+            ring_length_sd = sd(ring_length_µm, na.rm=TRUE),
+            vessel_size = mean(mean_vessel_size_µm2, na.rm=TRUE),
+            vessel_size_sd = sd(mean_vessel_size_µm2, na.rm=TRUE),
+            vessel_density = mean(vessel_density_per_mm2, na.rm=TRUE),
+            vessel_density_sd = sd(vessel_density_per_mm2, na.rm=TRUE),
+            Kth_mean = mean(Kth, na.rm=TRUE),
+            Kth_mean_sd = sd(Kth, na.rm=TRUE)
+            )
+anat_T_long <- anat_T %>%
+  pivot_longer(
+    cols = c(ring_length, vessel_size, vessel_density, Kth_mean),
+    names_to = "variable",
+    values_to = "value"
+  ) %>%
+  pivot_longer(
+    cols = c(ring_length_sd, vessel_size_sd, vessel_density_sd, Kth_mean_sd),
+    names_to = "sd_variable",
+    values_to = "sd"
+  ) %>%
+  filter(sub("_sd$", "", sd_variable) == variable)  # Match each variable with its correct SD
+
+
+ggplot(anat_T_long, aes(x = year, y = value, color = site_type, group = site_type)) +
+  geom_line(na.rm = TRUE) +
+  geom_point(size = 2, na.rm = TRUE) +
+  geom_errorbar(aes(ymin = value - sd, ymax = value + sd), width = 0.2, na.rm = TRUE) +
+  facet_grid(variable ~ species, scales = "free_y") +
+  labs(x = "Year", y = "Measurement Value", color = "Site Type") +
+  theme_minimal()
+
+
+anat_R <- anat_0to1[names(anat_sp_organ) %>% str_detect("_R")] %>%
+  bind_rows(.id = "source")
+
+anat_R <- anat_R %>%
+  group_by(site_type, species, year) %>%
+  summarize(ring_length = mean(ring_length_µm, na.rm=TRUE),
+            ring_length_sd = sd(ring_length_µm, na.rm=TRUE),
+            vessel_size = mean(mean_vessel_size_µm2, na.rm=TRUE),
+            vessel_size_sd = sd(mean_vessel_size_µm2, na.rm=TRUE),
+            vessel_density = mean(vessel_density_per_mm2, na.rm=TRUE),
+            vessel_density_sd = sd(vessel_density_per_mm2, na.rm=TRUE),
+            Kth_mean = mean(Kth, na.rm=TRUE),
+            Kth_mean_sd = sd(Kth, na.rm=TRUE)
+  )
+anat_R_long <- anat_R %>%
+  pivot_longer(
+    cols = c(ring_length, vessel_size, vessel_density, Kth_mean),
+    names_to = "variable",
+    values_to = "value"
+  ) %>%
+  pivot_longer(
+    cols = c(ring_length_sd, vessel_size_sd, vessel_density_sd, Kth_mean_sd),
+    names_to = "sd_variable",
+    values_to = "sd"
+  ) %>%
+  filter(sub("_sd$", "", sd_variable) == variable)  # Match each variable with its correct SD
+
+
+
+ggplot(anat_R_long, aes(x = year, y = value, color = site_type, group = site_type)) +
+  geom_line(na.rm = TRUE) +
+  geom_point(size = 2, na.rm = TRUE) +
+  geom_errorbar(aes(ymin = value - sd, ymax = value + sd), width = 0.2, na.rm = TRUE) +
+  facet_grid(variable ~ species, scales = "free_y") +
+  labs(x = "Year", y = "Measurement Value", color = "Site Type") +
+  theme_minimal()
+
 
